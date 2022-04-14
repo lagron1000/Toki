a6c33206bacd346a826b2257f18793bea6ba41cc
lagron1000 <lagron1000@gmail.com>
2022-04-14 20:39:58 +0300

    done with contacts visuals for now

--- src/Components/Chat/Contacts/Contact.js
+++ src/Components/Chat/Contacts/Contact.js
@@ -3,8 +3,8 @@ function Contact({name, img}) {
 
 
     return(
-        <div>
-            <p>{name}</p>
+        <div className="card table-hover">
+            <p class="card-header mb-3" >{name}</p>
             <img className="pfp mb-3" src={img}></img>
         </div>
     )
