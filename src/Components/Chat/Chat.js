7cd009240d18dd9533403a296b1b4c11026d8c01
lagron1000 <lagron1000@gmail.com>
2022-04-20 18:36:39 +0300

    making sure

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import './Chat.css';
 import Contacts from './Contacts/Contacts'
 import InputBar from './messages/InputBar';
 import Messages from './messages/messages';
 function Chat(user) {
+
+    const [currentChat, chatStateSwitch] = useState(null);
+
+    const chooseChat = (chat) => {
+        chatStateSwitch(chat)
+    }
+
     return (
         <div>
             <div class="row g-0 no-gutters">
                 <div id='contacts' class="column">
-                    <Contacts user = {user}></Contacts>
+                    <Contacts user = {user} chooseChat = {chooseChat}></Contacts>
                 </div>
                 <div id='chat' class="column">
                     {/* <Messages ch={user.user.chatHistory}></Messages> */}
