919a8e7a293ab53b4c8b1afa2837f3225280902d
lagron1000 <lagron1000@gmail.com>
2022-04-23 17:25:11 +0300

    accidently pushed wrong

--- src/Components/Chat/Contacts/Contact.js
+++ src/Components/Chat/Contacts/Contact.js
@@ -1,20 +1,20 @@
 function Contact({ friend, img, lastMsg, meObj }) {
 
-    // var msgContent = lastMsg ? lastMsg.content : "";
+    var msgContent = lastMsg ? lastMsg.content : "";
 
     var msgSender
-    // if (lastMsg != null) msgSender = lastMsg.me ? meObj.user.name + ":" : friend.name + ":";
-    // else msgSender = ""
+    if (lastMsg != null) msgSender = lastMsg.me ? meObj.user.name + ":" : friend.name + ":";
+    else msgSender = ""
 
-    // var timeStamp = lastMsg ? lastMsg.time : "";
+    var timeStamp = lastMsg ? lastMsg.time : "";
 
 
     return (
         <div className="card table-hover">
-            {/* <p class="card-header mb-3" >{friend.name}</p> */}
+            <p class="card-header mb-3" >{friend.name}</p>
             <div className="content">
                 <img className="pfp mb-3" src={img}></img>
-                {/* <p className="cardText">{msgSender} {msgContent} {timeStamp}</p> */}
+                <p className="cardText">{msgSender} {msgContent} {timeStamp}</p>
             </div>
         </div>
     )
