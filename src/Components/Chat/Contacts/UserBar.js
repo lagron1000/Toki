78ac99a4b8f7e3af082b6142ad45b3f44fdbdee7
lagron1000 <lagron1000@gmail.com>
2022-04-28 18:19:23 +0300

    visual touches

--- src/Components/Chat/Contacts/UserBar.js
+++ src/Components/Chat/Contacts/UserBar.js
@@ -7,9 +7,8 @@ function UserBar(props) {
 
     return (
         <div>
-            <button onClick={()=>{setShowPopup(true)}} >Add Contact</button>
+            <button className="btn btn-secondary mb-3" onClick={()=>{setShowPopup(true)}} >Add Contact</button>
             <AddFriend chooseChat={props.chooseChat} user={props.user} trigger={showPopup} setShowPopup={setShowPopup} ></AddFriend>
-            <input type={"text"}></input>
         </div>
     )
 }
